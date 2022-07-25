48b68e54921023c8a2c075f32f63389fa0ce4d76
Pug <batterypug@gmail.com>
2022-07-25 22:41:21 +1000

    Add parameter names

--- Common/src/main/java/com/github/merchantpug/bovinesandbuttercups/mixin/SuspiciousStewRecipeMixin.java
+++ Common/src/main/java/com/github/merchantpug/bovinesandbuttercups/mixin/SuspiciousStewRecipeMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(SuspiciousStewRecipe.class)
 public class SuspiciousStewRecipeMixin {
     @Inject(method = "matches(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/level/Level;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/tags/TagKey;)Z", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void returnFalseIfMoobloomFlower(CraftingContainer $$0, Level $$1, CallbackInfoReturnable<Boolean> cir, boolean hasFlower, boolean hasRedMushroom, boolean hasBrownMushroom, boolean hasBowl, int i, ItemStack itemStack) {
+    private void returnFalseIfMoobloomFlower(CraftingContainer container, Level level, CallbackInfoReturnable<Boolean> cir, boolean hasFlower, boolean hasRedMushroom, boolean hasBrownMushroom, boolean hasBowl, int i, ItemStack itemStack) {
         if (itemStack.is(BovineTags.SUSPICIOUS_STEW_RECIPE_EXCLUDED)) {
             cir.setReturnValue(false);
         }

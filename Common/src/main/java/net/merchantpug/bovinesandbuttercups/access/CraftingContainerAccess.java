312d4f6b4b9432b23f495d3aa27ba8047d467e19
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-01-24 13:30:08 +1100

    Remove unused mixins

--- Common/src/main/java/net/merchantpug/bovinesandbuttercups/access/CraftingContainerAccess.java
+++
@@ -1,8 +0,0 @@
-package net.merchantpug.bovinesandbuttercups.access;
-
-import net.minecraft.world.level.Level;
-
-public interface CraftingContainerAccess {
-    Level bovinesandbuttercups$getLevel();
-    void bovinesandbuttercups$setLevel(Level level);
-}

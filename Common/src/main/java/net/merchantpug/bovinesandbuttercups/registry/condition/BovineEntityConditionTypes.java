05bac41d4ed73842d9a115985d36aaffe8fa3fe9
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-02-28 23:11:13 +1100

    Change EntityTypeCondition to PredicateCondition

--- Common/src/main/java/net/merchantpug/bovinesandbuttercups/registry/condition/BovineEntityConditionTypes.java
+++ Common/src/main/java/net/merchantpug/bovinesandbuttercups/registry/condition/BovineEntityConditionTypes.java
@@ -2,8 +2,6 @@ package net.merchantpug.bovinesandbuttercups.registry.condition;
 
 import net.merchantpug.bovinesandbuttercups.BovinesAndButtercups;
 import net.merchantpug.bovinesandbuttercups.api.condition.ConditionConfiguration;
-import net.merchantpug.bovinesandbuttercups.api.condition.block.BlockConditionType;
-import net.merchantpug.bovinesandbuttercups.api.condition.block.BlockConfiguredCondition;
 import net.merchantpug.bovinesandbuttercups.api.condition.data.meta.ConstantConditionConfiguration;
 import net.merchantpug.bovinesandbuttercups.api.condition.data.meta.NotConditionConfiguration;
 import net.merchantpug.bovinesandbuttercups.api.condition.entity.EntityConditionType;
@@ -12,12 +10,11 @@ import net.merchantpug.bovinesandbuttercups.api.condition.data.meta.AndCondition
 import net.merchantpug.bovinesandbuttercups.api.condition.data.meta.OrConditionConfiguration;
 import net.merchantpug.bovinesandbuttercups.data.condition.entity.BlocksInRadiusCondition;
 import net.merchantpug.bovinesandbuttercups.data.condition.entity.EntitiesInRadiusCondition;
-import net.merchantpug.bovinesandbuttercups.data.condition.entity.EntityTypeCondition;
+import net.merchantpug.bovinesandbuttercups.data.condition.entity.PredicateCondition;
 import net.merchantpug.bovinesandbuttercups.registry.BovineRegistryKeys;
 import net.merchantpug.bovinesandbuttercups.registry.RegistrationProvider;
 import net.merchantpug.bovinesandbuttercups.registry.RegistryObject;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.state.pattern.BlockInWorld;
 
 import java.util.function.Supplier;
 
@@ -30,7 +27,7 @@ public class BovineEntityConditionTypes {
     public static final RegistryObject<EntityConditionType<OrConditionConfiguration<Entity>>> OR = register("or", () -> new EntityConditionType<>(OrConditionConfiguration.getCodec(EntityConfiguredCondition.CODEC)));
     public static final RegistryObject<EntityConditionType<BlocksInRadiusCondition>> BLOCKS_IN_RADIUS = register("blocks_in_radius", () -> new EntityConditionType<>(BlocksInRadiusCondition.CODEC));
     public static final RegistryObject<EntityConditionType<EntitiesInRadiusCondition>> ENTITIES_IN_RADIUS = register("entities_in_radius", () -> new EntityConditionType<>(EntitiesInRadiusCondition.CODEC));
-    public static final RegistryObject<EntityConditionType<EntityTypeCondition>> ENTITY_TYPE = register("entity_type", () -> new EntityConditionType<>(EntityTypeCondition.CODEC));
+    public static final RegistryObject<EntityConditionType<PredicateCondition>> PREDICATE = register("predicate", () -> new EntityConditionType<>(PredicateCondition.CODEC));
 
     public static void register() {
 

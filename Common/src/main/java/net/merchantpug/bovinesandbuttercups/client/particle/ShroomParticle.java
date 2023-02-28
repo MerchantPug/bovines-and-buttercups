4762f6eb7d51fb0738e36017ec91b696480d1cec
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-02-28 21:00:27 +1100

    Give spawned particles a random delta.

--- Common/src/main/java/net/merchantpug/bovinesandbuttercups/client/particle/ShroomParticle.java
+++ Common/src/main/java/net/merchantpug/bovinesandbuttercups/client/particle/ShroomParticle.java
@@ -6,13 +6,16 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 
 public class ShroomParticle extends TextureSheetParticle {
-    protected ShroomParticle(ClientLevel level, SpriteSet spriteSet, double x, double y, double z, Vector3f color) {
+    protected ShroomParticle(ClientLevel level, SpriteSet spriteSet, double x, double y, double z, double xd, double yd, double zd, Vector3f color) {
         super(level, x, y, z);
         this.setSize(0.01F, 0.01F);
         this.pickSprite(spriteSet);
         this.setColor(color.x(), color.y(), color.z());
         this.quadSize *= this.random.nextFloat() * 0.6F + 0.6F;
         this.lifetime = (int)(16.0D / (Math.random() * 0.8D + 0.2D));
+        this.xd = xd;
+        this.yd = yd;
+        this.zd = zd;
         this.hasPhysics = false;
         this.friction = 1.0F;
         this.gravity = 0.0F;
@@ -32,7 +35,7 @@ public class ShroomParticle extends TextureSheetParticle {
 
         public Particle createParticle(ShroomParticleOptions type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
             Vector3f color = type.color();
-            return new ShroomParticle(level, spriteSet, x, y, z, color);
+            return new ShroomParticle(level, spriteSet, x, y, z, xSpeed, ySpeed, zSpeed, color);
         }
     }
 }

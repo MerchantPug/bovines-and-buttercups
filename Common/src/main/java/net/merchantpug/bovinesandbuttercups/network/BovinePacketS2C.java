3d38401e67d383e8677166e9dc050cee8cb93826
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-03-03 19:53:17 +1100

    Fix serversided crash involving packets on Forge servers

--- Common/src/main/java/net/merchantpug/bovinesandbuttercups/network/BovinePacketS2C.java
+++ Common/src/main/java/net/merchantpug/bovinesandbuttercups/network/BovinePacketS2C.java
@@ -1,7 +1,5 @@
 package net.merchantpug.bovinesandbuttercups.network;
 
-import net.minecraft.client.Minecraft;
-
 public interface BovinePacketS2C extends BovinePacket {
-    void handle(Minecraft minecraft);
+    void handle();
 }
--- Fabric/src/main/java/net/merchantpug/bovinesandbuttercups/network/BovinePacketsS2C.java
+++ Fabric/src/main/java/net/merchantpug/bovinesandbuttercups/network/BovinePacketsS2C.java
@@ -2,10 +2,9 @@ package net.merchantpug.bovinesandbuttercups.network;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.merchantpug.bovinesandbuttercups.network.s2c.SyncDatapackContentsPacket;
-import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 
-import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class BovinePacketsS2C {
@@ -13,7 +12,7 @@ public class BovinePacketsS2C {
         ClientPlayNetworking.registerGlobalReceiver(SyncDatapackContentsPacket.ID, createS2CHandler(SyncDatapackContentsPacket::decode, SyncDatapackContentsPacket::handle));
     }
 
-    public static <T extends BovinePacket> ClientPlayNetworking.PlayChannelHandler createS2CHandler(Function<FriendlyByteBuf, T> decode, BiConsumer<T, Minecraft> handler) {
-        return (client, _handler, buf, responseSender) -> handler.accept(decode.apply(buf), client);
+    public static <T extends BovinePacket> ClientPlayNetworking.PlayChannelHandler createS2CHandler(Function<FriendlyByteBuf, T> decode, Consumer<T> handler) {
+        return (client, _handler, buf, responseSender) -> handler.accept(decode.apply(buf));
     }
 }

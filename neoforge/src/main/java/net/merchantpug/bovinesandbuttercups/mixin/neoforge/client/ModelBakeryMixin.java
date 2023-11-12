edf41c4ea354bacbd92e1c5f00fefaf4749c6fe4
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-11-13 00:17:45 +1100

    Remove unnecessary mixins from NeoForge module.

--- neoforge/src/main/java/net/merchantpug/bovinesandbuttercups/mixin/neoforge/client/ModelBakeryMixin.java
+++ neoforge/src/main/java/net/merchantpug/bovinesandbuttercups/mixin/neoforge/client/ModelBakeryMixin.java
@@ -1,16 +1,13 @@
 package net.merchantpug.bovinesandbuttercups.mixin.neoforge.client;
 
-import net.merchantpug.bovinesandbuttercups.access.ModelBakeryAccess;
 import net.merchantpug.bovinesandbuttercups.client.util.BovineStateModelUtil;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.model.AtlasSet;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Map;
 
 @Mixin(ModelBakery.class)
-public abstract class ModelBakeryMixin implements ModelBakeryAccess {
+public abstract class ModelBakeryMixin {
     @Shadow protected abstract void cacheAndQueueDependencies(ResourceLocation resourceLocation, UnbakedModel unbakedModel);
 
     @Inject(method = "loadModel", at = @At("HEAD"), cancellable = true)
@@ -28,24 +25,6 @@ public abstract class ModelBakeryMixin implements ModelBakeryAccess {
             cacheAndQueueDependencies(modelKey, model);
             ci.cancel();
         }
-    }
-
-    @Unique
-    Map<ResourceLocation, AtlasSet.StitchResult> bovinesandbuttercups$stitchResults = null;
-
-    @Override
-    public Map<ResourceLocation, AtlasSet.StitchResult> bovinesandbuttercups$getStitchResults() {
-        return bovinesandbuttercups$stitchResults;
-    }
-
-    @Override
-    public void bovinesandbuttercups$setStitchResults(Map<ResourceLocation, AtlasSet.StitchResult> value) {
-        this.bovinesandbuttercups$stitchResults = value;
-    }
-
-    @Override
-    public void bovinesandbuttercups$resetStitchResults() {
-        bovinesandbuttercups$stitchResults = null;
     }
 
 }

710f721c4608f3550329cd886fe971e185d38c67
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-23 14:47:44 +1100

    Fix removed class import in BovineCreativeTabs.

--- neoforge/src/main/java/net/merchantpug/bovinesandbuttercups/registry/BovineCreativeTabs.java
+++ neoforge/src/main/java/net/merchantpug/bovinesandbuttercups/registry/BovineCreativeTabs.java
@@ -8,7 +8,6 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 

655960e477515c1f0e8fae53037b58bcd0afbad7
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-09 15:12:53 +1100

    Fixed Mooblooms not naturally spawning.

--- Common/src/main/java/net/merchantpug/bovinesandbuttercups/data/condition/biome/BiomeLocationCondition.java
+++ Common/src/main/java/net/merchantpug/bovinesandbuttercups/data/condition/biome/BiomeLocationCondition.java
@@ -1,15 +1,12 @@
 package net.merchantpug.bovinesandbuttercups.data.condition.biome;
 
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.merchantpug.bovinesandbuttercups.api.condition.ConditionConfiguration;
 import net.merchantpug.bovinesandbuttercups.util.CodecUtil;
+import net.merchantpug.bovinesandbuttercups.util.HolderUtil;
 import net.minecraft.core.Holder;
-import net.minecraft.core.HolderSet;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 
 public class BiomeLocationCondition extends ConditionConfiguration<Holder<Biome>> {
@@ -19,18 +16,18 @@ public class BiomeLocationCondition extends ConditionConfiguration<Holder<Biome>
         ).apply(builder, BiomeLocationCondition::new));
     }
 
-    private final HolderSet<Biome> biomes;
+    private final HolderUtil.PsuedoHolder<Biome> biomes;
 
-    public BiomeLocationCondition(HolderSet<Biome> biomes) {
+    public BiomeLocationCondition(HolderUtil.PsuedoHolder<Biome> biomes) {
         this.biomes = biomes;
     }
 
     @Override
     public boolean test(Holder<Biome> biome) {
-        return biomes.contains(biome);
+        return HolderUtil.containsBiomeHolder(biome, biomes);
     }
 
-    public HolderSet<Biome> getBiomes() {
+    public HolderUtil.PsuedoHolder<Biome> getBiomes() {
         return biomes;
     }
 }
